9b01158d832474f05924beecd53521eb1d93630b
Aquib77492 <98381907+Aquib77492@users.noreply.github.com>
2022-02-02 14:44:40 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,6 @@
  */
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello, my name is Mohd Aquib");
+    System.out.println("Hello, welcome");
   }
 }
